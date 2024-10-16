86c71a67e7ac8234f36ae8fd9007ec1a1ae57b48
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-10-16 15:36:02 -0400

    Cleaned my about me.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,19 +7,11 @@ const About = ({ toggle })=> {
             <h4>About me</h4>
             <div className="about-cont">
                 {/* <h4>About me</h4> */}
-                <p>My name is <span>Zelalem Fitru</span> but I go by Z.</p>
-                <p>
-                    I was a Communications Sergeant while serving in the <span>United Sates Army</span> with the 1/509th Infantry Regiment. 
-                </p>
-                <p>
-                    Attended a coding school program that lasts seven months at <span>Turing School of Software & Design</span>.
-                </p>
-                <p>
-                    If you would like to reach out or get more information about me, click on my resume <span>button</span>.
-                </p>
-                <p>
-                    I also have all my socials on the bottom.
-                </p>
+                <p>My name is <span>Zelalem Fitru</span>, but I go by Z.</p>
+                <p>I served as a Communications Sergeant in the <span>United States Army</span> with the 1/509th Infantry Regiment, where I developed strong leadership and communication skills.</p>
+                <p>I completed a seven-month coding program at the <span>Turing School of Software & Design</span>, where I honed my skills in web development.</p>
+                <p>If you’d like to learn more about me or my work, feel free to check out my resume by clicking the <span>button</span>.</p>
+                <p>You can also find my social media links at the bottom of the page.</p>
                 <a className='contact-btn' 
                     href='https://docs.google.com/document/d/1k16ioZACLJSKqUJD2KOa0le5yLaEXXOkMRRufpF8VDQ/edit?usp=sharing'
                     target='_black'
