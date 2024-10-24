fa391d437a2999f482182de5bf122b5d19cb94ef
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-10-24 11:32:45 -0400

    Added an email button in about me.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,13 +9,17 @@ const About = ({ toggle })=> {
                 <p>My name is <span>Zelalem Fitru</span>, but I go by Z.</p>
                 <p>I served as a Communications Sergeant in the <span>United States Army</span> with the 1/509th Infantry Regiment, where I developed strong leadership and communication skills.</p>
                 <p>I completed a seven-month coding program at the <span>Turing School of Software & Design</span>, where I honed my skills in web development.</p>
-                <p>If you’d like to learn more about me or my work, feel free to check out my resume by clicking the <span>button</span>.</p>
+                <p>If you’d like to learn more about me or my work, feel free to check out my resume by clicking <span>My Resume</span> button.</p>
+                <p>Or send me a message by clicking on the <span>Email Me</span> button.</p>
                 <p>You can also find my social media links at the bottom of the page.</p>
                 <a className='contact-btn' 
                     href='https://docs.google.com/document/d/1k16ioZACLJSKqUJD2KOa0le5yLaEXXOkMRRufpF8VDQ/edit?usp=sharing'
                     target='_black'
                     rel='noopener noreferrer'>
                     <button>My Resume</button>
+                </a>
+                <a href='mailto:zelalemtfitru@gmail.com'>
+                    <button>Email Me</button>
                 </a>
             </div>
         </section>
